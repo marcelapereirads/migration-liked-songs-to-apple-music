d15156fd495dc07c80148998d1fde7355ecd63fc
Marcela <marcelapereirads@gmail.com>
2024-06-12 23:44:43 +0100

    Navigate to Liked Songs page

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+  // Timeout of 10000 milliseconds defined due to the real requests
+  defaultCommandTimeout: 10000,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/copy-songs.cy.js
+++ cypress/e2e/copy-songs.cy.js
@@ -11,13 +11,23 @@ describe('Track songs from Spotify', () => {
       );
     }
 
-    cy.intercept('https://spclient.wg.spotify.com/ads/v2/config?market=PT', {});
+    cy.intercept('GET', 'https://spclient.wg.spotify.com/ads/v2/config?market=PT', {});
+    cy.intercept('GET', 'https://open.spotify.com/undefined/api/v1/config/undefined', {});
+
     cy.visit('https://open.spotify.com/');
+
+    cy.get('#onetrust-accept-btn-handler', { timeout: 10000 }).click();
 
     cy.get('[data-testid="login-button"]').click();
 
     cy.get('#login-username').type(username);
     cy.get('#login-password').type(password);
     cy.get('#login-button').click();
+
+    //TODO: to change the language to English
+
+    cy.contains('Your Library').click();
+    cy.get('[class^=LegacyChip]').contains('Music').click();
+    cy.get('a[title="Liked Songs"]').click({ force: true });
   });
 });

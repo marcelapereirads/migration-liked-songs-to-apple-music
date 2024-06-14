2d713970b3baf2036ae9ed1d259028040115fc6a
Marcela <marcelapereirads@gmail.com>
2024-06-14 20:22:40 +0100

    Set language to English

--- cypress/e2e/copy-songs.cy.js
+++ cypress/e2e/copy-songs.cy.js
@@ -16,17 +16,25 @@ describe('Track songs from Spotify', () => {
 
     cy.visit('https://open.spotify.com/');
 
+    //* Accept cookies
     cy.get('#onetrust-accept-btn-handler', { timeout: 10000 }).click();
 
+    //* Login
     cy.get('[data-testid="login-button"]').click();
-
     cy.get('#login-username').type(username);
     cy.get('#login-password').type(password);
     cy.get('#login-button').click();
 
-    //TODO: to change the language to English
+    //* Go to settings page and change language to English
+    cy.get('[data-testid=user-widget-avatar]').click();
+    cy.get('a[href="/preferences"]').click();
 
-    cy.contains('Your Library').click();
+    // Remove an invisible div over the language dropdown
+    cy.get('[data-testid="LayoutResizer__resize-bar"]').parent().invoke('remove');
+    cy.get('select > option[value="en"]').parent().select('en');
+    cy.get('[data-testid="top-bar-back-button"]').click();
+
+    //* Go to Liked Songs page
     cy.get('[class^=LegacyChip]').contains('Music').click();
     cy.get('a[title="Liked Songs"]').click({ force: true });
   });

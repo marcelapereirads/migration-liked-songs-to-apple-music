821d7f2e9a5a0ddc73f9bc9447eb3cc608cc37da
Marcela <marcelapereirads@gmail.com>
2024-09-13 20:07:34 +0100

    Get the artists

--- cypress/e2e/copy-songs.cy.js
+++ cypress/e2e/copy-songs.cy.js
@@ -16,17 +16,17 @@ describe('Track songs from Spotify', () => {
 
     cy.visit('https://open.spotify.com/');
 
-    //* Accept cookies
-    cy.get('#onetrust-accept-btn-handler', { timeout: 10000 }).click();
+    // Accept cookies
+    cy.get('#onetrust-accept-btn-handler').click();
 
-    //* Login
+    // Login
     cy.get('[data-testid="login-button"]').click();
     cy.get('#login-username').type(username);
     cy.get('#login-password').type(password);
     cy.get('#login-button').click();
 
-    //* Go to settings page and change language to English
-    cy.get('[data-testid=user-widget-avatar]', { timeout: 10000 }).click();
+    // Go to settings page and change language to English
+    cy.get('[data-testid=user-widget-avatar]').click();
     cy.get('a[href="/preferences"]').click();
 
     // Remove an invisible div over the language dropdown
@@ -34,13 +34,13 @@ describe('Track songs from Spotify', () => {
 
     cy.get('.encore-announcement-set button').click();
     cy.get('select > option[value="en"]').parent().select('en', { force: true });
-    cy.get('[data-testid="top-bar-back-button"]').click({ force: true });
+    cy.get('svg').contains('Spotify').first().click({ force: true });
 
-    //* Go to Liked Songs page
+    // Go to Liked Songs page
     cy.get('[class^=LegacyChip]').contains('Music').click();
     cy.get('a[title="Liked Songs"]').click({ force: true });
 
-    //* Get the amount of liked songs
+    // Get the amount of liked songs
     cy.contains('songs')
       .invoke('text')
       .then((text) => {
@@ -49,24 +49,33 @@ describe('Track songs from Spotify', () => {
 
         cy.get('[data-testid="top-sentinel"]').next().as('trackList');
 
-        //* For some reason, the list of songs in Spotify starts in 2
+        // For some reason, the list of songs in Spotify starts in 2
         for (let i = 1; i <= amountOfSongs; i++) {
-          //* For each row of the track list, get the text of the second column, containing song name and artist
+          // For each row of the track list, get the text of the second column, containing song name and artist
           cy.get('@trackList')
             .find(`[aria-rowindex=${i + 1}] > [data-testid="tracklist-row"] > [aria-colindex="2"]`)
             .as('song')
             .find('[data-testid="internal-track-link"]')
+            .as('songName')
             .invoke('text')
-            .then((text) => {
-              //* Slowly scroll down the list, to load more songs while gets the song names
-              const pixelsToScrollDown = 60;
+            .then(($songName) => {
+              // To the artist, it is not possible to find the next element from the song, because, in the case of explicit content,
+              // the 'E' icon is presented before the artist
+              cy.get('@song')
+                .find('a')
+                .eq(1)
+                .invoke('text')
+                .then(($artist) => {
+                  console.log(`${$songName} - ${$artist}`);
+                });
+
+              // Slowly scroll down the list, to load more songs while gets the song names
+              const pixelsToScrollDown = 55;
               accumulatedScroll += pixelsToScrollDown;
 
               if (i % 5 === 0) {
-                console.log('scroll');
                 cy.get('div[data-overlayscrollbars-viewport*=overflowYScroll]').scrollTo(0, accumulatedScroll);
               }
-              console.log(i, text);
             });
         }
       });
